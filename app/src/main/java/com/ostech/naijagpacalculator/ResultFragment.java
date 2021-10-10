a09b14a420aa3a838ebc33138db53b1c233bfd29
Osas-Solo <osas.solo@outlook.com>
2021-10-10 23:18:33 +0100

    Styled views

--- app/src/main/java/com/ostech/naijagpacalculator/ResultFragment.java
+++ app/src/main/java/com/ostech/naijagpacalculator/ResultFragment.java
@@ -11,6 +11,7 @@ import android.widget.TableRow;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -29,7 +30,6 @@ public class ResultFragment extends Fragment {
     private static final String TAG = ResultFragment.class.getCanonicalName();
 
     private RecyclerView semestersRecyclerView;
-    private ProgressBar cgpaProgressBar;
     private AppCompatTextView cgpaTextView;
     private AppCompatTextView tnuTextView;
     private AppCompatTextView tcpTextView;
@@ -48,7 +48,6 @@ public class ResultFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_result, container, false);
 
         semestersRecyclerView = view.findViewById(R.id.result_semester_recycler_view);
-        cgpaProgressBar = view.findViewById(R.id.result_cgpa_progress_bar);
         cgpaTextView = view.findViewById(R.id.result_cgpa_text_view);
         tnuTextView = view.findViewById(R.id.result_tnu_text_view);
         tcpTextView = view.findViewById(R.id.result_tcp_text_view);
@@ -63,12 +62,6 @@ public class ResultFragment extends Fragment {
         double totalGradePoint = AcademicRecord.getInstance().getTotalGradePoint();
         int numberOfCourses = AcademicRecord.getInstance().getNumberOfCourses();
         String remark = institution.getRemark(cgpa);
-
-        int cgpaProgressBarUpperRange = (int) (institution.MAXIMUM_GPA * 100);
-        int cgpaProgressBarValue = (int) (cgpa * 100);
-
-        cgpaProgressBar.setMax(cgpaProgressBarUpperRange);
-        cgpaProgressBar.setProgress(cgpaProgressBarValue);
 
         cgpaTextView.setText(String.format("%.2f", cgpa));
         tnuTextView.setText(String.format("%.0f", totalCreditUnit));
@@ -110,7 +103,6 @@ public class ResultFragment extends Fragment {
     }
 
     private class SemesterHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private ProgressBar gpaProgressBar;
         private AppCompatTextView gpaSemesterTextView;
         private AppCompatTextView gpaTextView;
         private AppCompatTextView tnuTextView;
@@ -124,7 +116,6 @@ public class ResultFragment extends Fragment {
             itemView.setOnClickListener(this);
 
             gpaSemesterTextView = itemView.findViewById(R.id.gpa_semester_text_view);
-            gpaProgressBar = itemView.findViewById(R.id.gpa_progress_bar);
             gpaTextView = itemView.findViewById(R.id.gpa_text_view);
             tnuTextView = itemView.findViewById(R.id.tnu_text_view);
             tcpTextView = itemView.findViewById(R.id.tcp_text_view);
@@ -144,12 +135,7 @@ public class ResultFragment extends Fragment {
             int numberOfCourses = semester.getNumberOfCourses();
             String remark = institution.getRemark(gpa);
 
-            int cgpaProgressBarUpperRange = (int) (institution.MAXIMUM_GPA * 100);
-            int cgpaProgressBarValue = (int) (gpa * 100);
-
             gpaSemesterTextView.setText(semester.getSemesterName());
-            gpaProgressBar.setMax(cgpaProgressBarUpperRange);
-            gpaProgressBar.setProgress(cgpaProgressBarValue);
 
             gpaTextView.setText(String.format("%.2f", gpa));
             tnuTextView.setText(String.format("%.0f", totalCreditUnit));
